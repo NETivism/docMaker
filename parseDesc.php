25721f7f6e97f2049945640439a89b16a6a4e99a
Sam Yang <samabc75@gmail.com>
2021-02-26 18:21:33 +0800

    refs #30694. parseDesc and parseParams prototype complete.

--- parseDesc.php
+++ parseDesc.php
@@ -6,6 +6,17 @@
  *   $baseDir => Directory path to base civicrm, like "/path/to/civicrm"
  *   $content => current full doc content
  */
+
+/**
+ * @start_document
+ * # Title 1
+ * ## Title 2
+ * jasdkfj
+ * sdfs
+ * ### Title 3
+ * @end_document
+ */
+
 function doParseDesc($params) {
   extract($params);
   print($entityName);
@@ -15,7 +26,21 @@ function doParseDesc($params) {
   print($content);
   print("\n");
 
-  $replaceDesc = "This is description...";
+  $docComments = array_filter(
+    token_get_all( file_get_contents( __FILE__ ) ), function($entry) {
+        return $entry[0] == T_DOC_COMMENT;
+    }
+  );
+
+  $replaceDesc = "";
+  foreach($docComments as $comments) {
+    $fileDocComment = $comments[1];
+    $fileDocComment = trim($fileDocComment, '/**');
+    $fileDocComment = str_replace(' * ', '', $fileDocComment);
+    preg_match('#@start_document(.+)@end_document#s',$fileDocComment,$matches);
+    $replaceDesc = $replaceDesc . $matches[1];
+  }
+  
   $search = "{{DESC}}";
   $content = str_replace($search, $replaceDesc, $content);
 
