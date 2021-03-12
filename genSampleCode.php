832029b272d31711b7991734c2db47ba1f4bd05f
Chang Shu-Huai <junsuwhy@gmail.com>
2021-03-12 18:47:41 +0800

    Refs #30695, Add function list capture code.

--- genSampleCode.php
+++ genSampleCode.php
@@ -8,5 +8,31 @@
  */
 function doGenSampleCode($params) {
   extract($params);
+
+  $fileName = $baseDir.'/tests/phpunit/api/v3/'.$entityName.'Test.php';
+
+  // Get Function name for Sample code.
+  $docBlocks = token_get_all( file_get_contents( $fileName ) );
+  $docComments = array_filter(
+    $docBlocks, function($entry) {
+        return $entry[0] == T_DOC_COMMENT;
+    }
+  );
+
+  $funcNames = array();
+  foreach($docComments as $order => $commentBlock) {
+    global $nextLine;
+    $nextLine = $docBlocks[$order+2][2];
+    $funcNameBlocks = array_filter(
+      $docBlocks, function($entry) {
+        global $nextLine;
+        return $entry[2] == $nextLine && $entry[0] == T_STRING;
+      }
+    );
+    $funcNameBlock = reset($funcNameBlocks);
+    $funcNames[] = $funcNameBlock[1];
+  }
+  $params['testFunctions'] = $funcNames;
+
   return $params;
 }
--- genTestResult.php
+++ genTestResult.php
@@ -9,8 +9,12 @@
 function doGenTestResult($params) {
   extract($params);
 
-  // chdir($baseDir.'/tests/phpunit');
-  // shell_exec("phpunit \"api/v3/{$entityName}Test.php\"");
+  // if ($testFunctions) {
+  //   foreach ($testFunctions as $functionName) {
+  //     chdir($baseDir.'/tests/phpunit');
+  //     shell_exec("phpunit --filter '/::{$functionName}$/' \"api/v3/{$entityName}Test.php\"");
+  //   }
+  // }
 
   return $params;
 }
