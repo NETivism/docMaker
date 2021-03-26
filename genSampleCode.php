62e5f4bd9a20315351f327c4690bc44fe3b1e4e5
Sam Yang <samabc75@gmail.com>
2021-03-26 18:07:15 +0800

    Refs #30695. Add get sample code description and phpunit test result.

--- genSampleCode.php
+++ genSampleCode.php
@@ -1,5 +1,5 @@
 <?php
-
+// ini_set('display_errors',"1");
 /**
  * in $params
  *   $entityName => entity name like "contact", "email"
@@ -9,9 +9,13 @@
 function doGenSampleCode($params) {
   extract($params);
 
+  $tokenMapping = array(
+    "{{API_KEY_HEADER}}" => "--header 'x-civicrm-api-key: <secret-key>'",
+    "{{SITE_KEY_HEADER}}" => "--header 'x-civicrm-site-key: <site-key>'"
+  );
+
   $fileName = $baseDir.'/tests/phpunit/api/v3/'.$entityName.'Test.php';
 
-  // Get Function name for Sample code.
   $docBlocks = token_get_all( file_get_contents( $fileName ) );
   $docComments = array_filter(
     $docBlocks, function($entry) {
@@ -19,8 +23,19 @@ function doGenSampleCode($params) {
     }
   );
 
-  $funcNames = array();
-  foreach($docComments as $order => $commentBlock) {
+  // Get sample code description.
+  $replaceDesc = "";
+  foreach($docComments as $order => $comments) {
+    $fileDocComment = $comments[1];
+    $fileDocComment = trim($fileDocComment, '/**');
+    $fileDocComment = str_replace(' * ', '', $fileDocComment);
+    preg_match('#@start_document(.+)@end_document#s',$fileDocComment,$matches);
+    $description = $matches[1];
+    foreach($tokenMapping as $token => $replacement) {
+      $description = str_replace($token, $replacement, $description);
+    }
+
+    // get function name and replace {{Result}} with unit test result.
     global $nextLine;
     $nextLine = $docBlocks[$order+2][2];
     $funcNameBlocks = array_filter(
@@ -30,9 +45,35 @@ function doGenSampleCode($params) {
       }
     );
     $funcNameBlock = reset($funcNameBlocks);
-    $funcNames[] = $funcNameBlock[1];
+    $funcName = $funcNameBlock[1];
+    $unitTestResultFileDir = $baseDir.'/docMaker/unit_test_results/'.$funcName.'Result.json';
+    $unitTestResultFile = fopen($unitTestResultFileDir, 'r');
+    $unitTestResult = fread($unitTestResultFile, filesize($unitTestResultFileDir));
+    fclose($unitTestResultFile);
+    $unitTestResult = "{literal}".$unitTestResult."{/literal}";
+    $description = str_replace("{{RESULT}}", $unitTestResult, $description);
+    $replaceDesc = $replaceDesc . $description;
   }
-  $params['testFunctions'] = $funcNames;
+
+  $search = "{{SAMPLE_CODE}}";
+  $content = str_replace($search, $replaceDesc, $content);
+  $params['content'] = $content;
+
+  // // Get Function name for Sample code.
+  // $funcNames = array();
+  // foreach($docComments as $order => $commentBlock) {
+  //   global $nextLine;
+  //   $nextLine = $docBlocks[$order+2][2];
+  //   $funcNameBlocks = array_filter(
+  //     $docBlocks, function($entry) {
+  //       global $nextLine;
+  //       return $entry[2] == $nextLine && $entry[0] == T_STRING;
+  //     }
+  //   );
+  //   $funcNameBlock = reset($funcNameBlocks);
+  //   $funcNames[] = $funcNameBlock[1];
+  // }
+  // $params['testFunctions'] = $funcNames;
 
   return $params;
 }
